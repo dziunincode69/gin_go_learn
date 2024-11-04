ab61cc3d765e19548c746751b7f1ae93ff8b41b8
dziunincode69 <d.ziunincode69@gmail.com>
2024-11-04 23:59:20 +0700

    feat: delete user

--- internal/routes/user.route.go
+++ internal/routes/user.route.go
@@ -7,7 +7,11 @@ import (
 	loginhandler "gin_go_learn/internal/handlers/login"
 	registerhandler "gin_go_learn/internal/handlers/register"
 	getuserhandler "gin_go_learn/internal/handlers/user"
+	"gin_go_learn/internal/helper"
 	"gin_go_learn/internal/middlewares"
+	"gin_go_learn/internal/models"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -29,6 +33,36 @@ func InitUserRoutes(db *gorm.DB, router *gin.Engine) {
 	apigroup := router.Group("/api")
 	apigroup.POST("/login", LoginHandler.HandleLogin)
 	apigroup.POST("/register", RegisterUserHandler.RegisterHandler)
-	protected := apigroup.Use(middlewares.JwtAuthMiddleware(), GetUserHandler.HandleGetUser)
-	protected.GET("/user/:id")
+	protected := apigroup.Use(middlewares.JwtAuthMiddleware())
+	protected.GET("/user/:id", GetUserHandler.HandleGetUser)
+	protected.DELETE("/user/:id", func(c *gin.Context) {
+		idparam := c.Param("id")
+		idparamint, err := strconv.Atoi(idparam)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		val, exist := c.Get("user")
+		if !exist {
+			c.JSON(400, gin.H{
+				"error": "data not exist",
+			})
+			return
+		}
+		valtostruct := val.(*helper.MyAppClaims)
+		if idparamint == valtostruct.ID {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "cannot delete your account",
+			})
+			return
+		}
+		var user models.User
+		db.Unscoped().Delete(&user, idparamint)
+		c.JSON(200, gin.H{
+			"status": "success",
+		})
+	})
+
 }
